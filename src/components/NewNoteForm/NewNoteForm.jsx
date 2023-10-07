bf48b64a29cb48ffe725bfe11bc842185f77b176
ngpadovan <ngpadovan@gmail.com>
2023-10-07 08:47:49 -0700

    inititial commit

---
+++ src/components/NewNoteForm/NewNoteForm.jsx
@@ -0,0 +1,8 @@
+// import { useState } from "react";
+
+
+export default function NewNoteForm () {
+    return (
+        <h1>newnoteform</h1>
+    )
+}
---
+++ src/pages/NoteList/NoteList.jsx
@@ -0,0 +1,12 @@
+// import { useState, useEffect } from "react";
+// import * as notesAPI from "../../utilities/notes-api";
+import NewNoteForm from "../../components/NewNoteForm/NewNoteForm";
+
+export default function NoteList() {
+
+
+    return (
+        <NewNoteForm />
+    )
+
+}
