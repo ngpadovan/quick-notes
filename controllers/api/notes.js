b4502d8542d5299615a3a566314f5d5996fa9a49
ngpadovan <ngpadovan@gmail.com>
2023-10-07 10:57:42 -0700

    adds model

--- controllers/api/notes.js
+++ controllers/api/notes.js
@@ -1,1 +1,10 @@
 const Note = require('../../models/note');
+
+module.exports = {
+    create
+}
+
+async function create(req,res) {
+    const note = await Note.create(req.body);
+    res.json(note);
+  }
---
+++ routes/api/notes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const notesCtrl = require('../../controllers/api/notes');
+
+router.post('/', notesCtrl.create);
+
+
+module.exports = router;
