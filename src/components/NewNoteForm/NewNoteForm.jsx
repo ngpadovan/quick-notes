f616eee3f346ce34f412d5c2831103667c16bb5b
ngpadovan <ngpadovan@gmail.com>
2023-10-07 08:57:18 -0700

    adds newnoteform component

--- src/components/NewNoteForm/NewNoteForm.jsx
+++ src/components/NewNoteForm/NewNoteForm.jsx
@@ -1,8 +1,21 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 
 export default function NewNoteForm () {
+const [newNote, setNewNote] = useState({
+
+})
+
+function handleChange(evt) {
+    setNewNote({ ...newNote, [evt.target.name]: evt.target.value });
+  }
+  
     return (
+        <>
         <h1>newnoteform</h1>
+        <input name="text" type="textarea"
+        onChange={handleChange} value={NewNoteForm.text} />
+
+        </>
     )
 }
